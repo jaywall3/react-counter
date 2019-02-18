2959b3e75bb1931b2bcef6f08d394c7fe0880f4b
Jay Wall <jay.wall@icloud.com>
2019-02-18 09:54:09 -0500

    reset works, no label...don't know how to save count values

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import Button from './components/Button'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-import './styles.css'
+import Button from "./components/Button";
+
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const incrementCount = increment => {
-    setCount(count + increment)
-  }
+    setCount(count + increment);
+  };
 
   return (
     <div className="App">
@@ -17,10 +18,22 @@ function App() {
       <Button increment={10} onClickFunction={incrementCount} />
       <Button increment={100} onClickFunction={incrementCount} />
       <Button increment={1000} onClickFunction={incrementCount} />
+      <Button increment={10000} onClickFunction={incrementCount} />
+      <Button increment={-1} onClickFunction={incrementCount} />
+      <Button increment={-10} onClickFunction={incrementCount} />
+      <Button increment={-100} onClickFunction={incrementCount} />
+      <Button increment={-1000} onClickFunction={incrementCount} />
+      <Button increment={-10000} onClickFunction={incrementCount} />
+      <Button increment={-count} onClickFunction={incrementCount} />
+
       <span>{count}</span>
     </div>
-  )
+  );
 }
+// Tried to label button as "Reset" - didn't work
+// Reset functionality works
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+// no idea how to save the count
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
